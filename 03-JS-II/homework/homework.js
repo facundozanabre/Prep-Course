64d89faa4fa5d4e96f23808bfe19ee539a3d115a
facundozanabre <fzanabre@gmail.com>
2022-06-26 16:50:17 -0300

    actualizacion test 2 2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -33,11 +33,12 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
-while (status = 1)
-console.log("Online");
-while (status = 2)
-console.log("Away");
-console.log("Offline");
+if (status = 1)
+return "Online";
+if (status = 2)
+return "Away";
+else
+return "Offline";
 }
 
 function saludo(idioma) {
@@ -47,7 +48,7 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-  
+
 }
 
 function colors(color) {
