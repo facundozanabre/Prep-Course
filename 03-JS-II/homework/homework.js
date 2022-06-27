0167e5048edba82fe71ca6dd148281b014aa3d21
facundozanabre <fzanabre@gmail.com>
2022-06-26 21:10:51 -0300

    actualizacion test 2 16

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -132,19 +132,21 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
-  if (num1 > num2 && num1 > num3 && num1 > 0)
-    return "Número 1 es mayor y positivo";
-  if (num1 < 0 || num2 < 0 || num3 < 0)
+  
+  
+  if (num1 === 0 || num2 === 0 || num3 === 0){
+  return "Error"; 
+  }else if (num1 < 0 || num2 < 0 || num3 < 0){
     return "Hay negativos";
-  var numero3 = num;
-  if (num3 > num1 && num3 > num2)
+  }else if (num1 > num2 && num1 > num3 && num1 > 0){
+    return "Número 1 es mayor y positivo";
+  }else if (num3 > num1 && num3 > num2){
+    var numero3 = num;
   numero3 = num3++;
   return numero3;
-  if (num1 === 0 || num2 === 0 || num3 === 0)
-  return "Error"; 
-  else
+  }else{
     return "false";
-  
+  }
 }
 
 function esPrimo(numero) {
