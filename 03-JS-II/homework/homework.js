d85b202985031d68b96c9331a9aa429dbb35234f
facundozanabre <fzanabre@gmail.com>
2022-06-30 20:14:34 -0300

    actualizacion test 2 24

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -190,7 +190,7 @@ function tablaDelSeis(){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
 let array = []
-  for (let i = 1; i < 11; i++){
+  for (let i = 0; i < 11; i++){
    array.push (6 * i);
   }
 return array;
