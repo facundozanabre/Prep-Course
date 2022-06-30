82c0271a4e9886f44345cce5a47e5f6179f13f57
facundozanabre <fzanabre@gmail.com>
2022-06-30 19:47:01 -0300

    actualizacion test 2 21

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -107,7 +107,12 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-
+if (numero % 1 === 0){
+  return "true";
+}
+  else{
+  return false;
+}
 }
 
 function fizzBuzz(numero) {
