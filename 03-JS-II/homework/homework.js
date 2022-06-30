7c90d8f7baa16e05207e9955eff4e7c18f022383
facundozanabre <fzanabre@gmail.com>
2022-06-30 19:04:51 -0300

    actualizacion test 2 20

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -142,7 +142,7 @@ function operadoresLogicos(num1, num2, num3) {
     return "Número 1 es mayor y positivo";
   }else if (num3 > num1 && num3 > num2){
     var numero3 = num3;
-  numero3 = num3++;
+  numero3 = num3 + 1;
   return numero3;
   }else{
     return false;
