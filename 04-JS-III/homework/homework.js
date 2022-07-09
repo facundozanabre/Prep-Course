19856061434850a254168ddb220374cbedca56f8
facundozanabre <fzanabre@gmail.com>
2022-07-08 22:00:04 -0300

    actualizacion test 3 25

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -81,6 +81,15 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+
+  for (var i = 0; i < array.length; i++){
+     if ( array[i] === elemento){
+      return "true";
+     } else {
+      return "false";
+     }
+
+     }
 }
 
 
